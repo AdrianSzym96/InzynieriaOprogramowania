9b3a540346115c3d4b0e937fbb60b01725ef3387
unknown <adrianszym96@gmail.com>
2022-03-17 23:43:08 +0100

    Changing UI text

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -8,6 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>
+            TY KURWO ZOSTAW KIBICA
+          </p>
+
         </p>
         <a
           className="App-link"
