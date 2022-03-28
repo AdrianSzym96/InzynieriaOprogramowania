b732495c6f59a7eb239b0da14d5b347fced8e73c
kat may <kat.chaotik@gmail.com>
2022-03-28 21:27:46 +0200

    test

--- Code/my-app/src/App.js
+++ Code/my-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <img src={kibic} className="Kibic-logo" alt="kibic" />
         <p>
+          hello world
         </p>
         <a>
           
