eeb3fdd133dc162e4413868cf4c4244d8ba5d854
adrianszym96@gmail.com <adrianszym96@gmail.com>
2022-03-29 19:32:12 +0200

    first draft of calculator

--- Code/my-app/src/components/CardItem.js
+++ Code/my-app/src/components/CardItem.js
@@ -7,7 +7,7 @@ function CardItem(props) {
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
+             <img
               className='cards__item__img'
               alt='Travel Image'
               src={props.src}
