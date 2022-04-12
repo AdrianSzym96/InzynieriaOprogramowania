a6c15cea9d1b52c6e315ca04f272688557304cd5
AdrianSzym96 <36892809+AdrianSzym96@users.noreply.github.com>
2022-04-12 12:38:19 +0200

    Update HeroSection.js

--- Code/my-app/src/components/HeroSection.js
+++ Code/my-app/src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>EXPOSURE CALCULATOR</h1>
-      <p>What are you waiting for?????</p>
+      <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button
           className='btns'
